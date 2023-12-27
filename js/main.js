089a8ed9f721b3d7ec1d660a1968621960dc4ee1
kristinatsikal <146119877+kristinatsikal@users.noreply.github.com>
2023-12-27 14:26:18 +0200

    add changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
-    renderItems();
-  });
-  
-  function openCreateForm() {
-    const createForm = document.getElementById('createForm');
-    createForm.style.display = 'block';
-  }
-  
-  window.openCreateForm = openCreateForm;
-  window.createNewItem = createNewItem;
+document.addEventListener('DOMContentLoaded', function() {
+  renderItems();
+});
+
+function openCreateForm() {
+  const createForm = document.getElementById('createForm');
+  createForm.style.display = 'block';
+}
+
+window.openCreateForm = openCreateForm;
+window.createNewItem = createNewItem;
+
+
--- js/operations.js
+++ js/operations.js
@@ -1,122 +1,181 @@
+let itemsArray = [
+  { name: 'Футболка з довгим рукавом', price: 20, category: 'Футболки' },
+  { name: 'Футболка з коротким рукавом', price: 25, category: 'Футболки' },
+  { name: 'Джинси', price: 40, category: 'Штани' },
+  { name: 'Легінси', price: 30, category: 'Штани' },
+  { name: 'Пальто', price: 60, category: 'Верхній одяг' },
+  { name: 'Куртка', price: 45, category: 'Верхній одяг' },
+  { name: 'Дублянка', price: 70, category: 'Верхній одяг' },
+  // і так далі...
+];
+
+const itemsKey = 'items';
+
 function getItems() {
-    return JSON.parse(localStorage.getItem('items')) || [];
-  }
-  
-  function setItems(items) {
-    localStorage.setItem('items', JSON.stringify(items));
-  }
-  
-  function renderItems(filteredItems) {
-    const itemList = document.getElementById('itemList');
-    itemList.innerHTML = ''; 
-  
-    const items = filteredItems || getItems();
-  
-    items.forEach((item, index) => {
-      const li = document.createElement('li');
-      li.textContent = `${item.name} - $${item.price}`;
-      const editButton = document.createElement('button');
-      editButton.textContent = 'Edit';
-      editButton.onclick = () => openEditForm(index);
-      li.appendChild(editButton);
-  
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = 'Delete';
-      deleteButton.onclick = () => confirmDeleteItem(index);
-      li.appendChild(deleteButton);
-  
-      itemList.appendChild(li);
-    });
-  }
-  
-  function openEditForm(index) {
-    const items = getItems();
-    const item = items[index];
-  
-    const itemName = prompt('Enter new name:', item.name);
-    const itemPrice = prompt('Enter new price:', item.price);
-  
-    if (itemName !== null && itemPrice !== null) {
-      items[index] = { name: itemName, price: parseFloat(itemPrice) };
-      setItems(items);
-      renderItems();
-    }
-  }
-  
+  const storedItems = localStorage.getItem(itemsKey);
+  return storedItems ? JSON.parse(storedItems) : [];
+}
 
-  function confirmDeleteItem(index) {
-    const confirmation = confirm('Are you sure you want to delete this item?');
-  
-    if (confirmation) {
-      deleteItem(index);
-    }
-  }
-  
+function setItems(items) {
+  localStorage.setItem(itemsKey, JSON.stringify(items));
+}
 
-  function deleteItem(index) {
+function getSelectedItem() {
+  // Реалізуйте логіку отримання обраного елемента (ви можете використовувати id, наприклад)
+  // Повертаєте елемент для редагування
+}
+
+function editItem(item, newName, newPrice) {
+  item.name = newName;
+  item.price = parseFloat(newPrice);
+  setItems(getItems());
+  renderItems();
+}
+
+function renderItems(filteredItems) {
+  const itemsContainer = document.getElementById('itemList');
+  const categoryListContainer = document.getElementById('categoryList');
+
+  itemsContainer.innerHTML = '';
+  categoryListContainer.innerHTML = '';
+
+  const items = filteredItems || getItems();
+  const categories = getCategories(items);
+
+  categories.forEach(category => {
+    const li = document.createElement('li');
+    li.textContent = category;
+    li.onclick = () => filterByCategory(category);
+    categoryListContainer.appendChild(li);
+  });
+
+  items.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = `${item.name} - $${item.price}`;
+
+    const editButton = createButton('Edit', () => openEditForm(item));
+    li.appendChild(editButton);
+
+    const deleteButton = createButton('Delete', () => confirmDeleteItem(item));
+    li.appendChild(deleteButton);
+
+    itemsContainer.appendChild(li);
+  });
+}
+
+function getCategories(items) {
+  const categoriesSet = new Set(items.map(item => item.category));
+  return Array.from(categoriesSet);
+}
+
+function createButton(text, clickHandler) {
+  const button = document.createElement('button');
+  button.textContent = text;
+  button.onclick = clickHandler;
+  return button;
+}
+
+function openEditForm(item) {
+  const editForm = document.getElementById('editForm');
+  document.getElementById('editItemName').value = item.name;
+  document.getElementById('editItemPrice').value = item.price;
+  editForm.style.display = 'block';
+}
+
+function saveEditedItem() {
+  const itemName = document.getElementById('editItemName').value;
+  const itemPrice = document.getElementById('editItemPrice').value;
+
+  // Отримайте поточний вибраний товар для редагування
+  const selectedItem = getSelectedItem();
+
+  // Викличте функцію редагування
+  editItem(selectedItem, itemName, itemPrice);
+
+  closeEditForm();
+}
+
+function filterByCategory(category) {
+  const items = getItems();
+  const filteredItems = items.filter(item => item.category === category);
+  renderItems(filteredItems);
+}
+
+function confirmDeleteItem(item) {
+  const confirmation = confirm('Are you sure you want to delete this item?');
+
+  if (confirmation) {
+    deleteItem(item);
+  }
+}
+
+function deleteItem(item) {
+  const items = getItems();
+  const updatedItems = items.filter(i => !isEqual(i, item));
+  console.log('Deleting item:', item);
+  setItems(updatedItems);
+  renderItems();
+}
+
+function isEqual(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
+}
+
+function createNewItem() {
+  const itemNameInput = document.getElementById('itemName');
+  const itemPriceInput = document.getElementById('itemPrice');
+  const itemCategoryInput = document.getElementById('itemCategory');
+
+  const itemName = itemNameInput.value;
+  const itemPrice = parseFloat(itemPriceInput.value);
+  const itemCategory = itemCategoryInput.value;
+
+  if (itemName && !isNaN(itemPrice) && itemCategory) {
+    const newItem = { name: itemName, price: itemPrice, category: itemCategory };
     const items = getItems();
-    items.splice(index, 1);
+    items.push(newItem);
     setItems(items);
     renderItems();
-  }
-  
 
-  function createNewItem() {
-    const itemName = document.getElementById('itemName').value;
-    const itemPrice = document.getElementById('itemPrice').value;
-  
-    if (itemName && itemPrice) {
-      const newItem = { name: itemName, price: parseFloat(itemPrice) };
-      const items = getItems();
-      items.push(newItem);
-      setItems(items);
-      renderItems();
-  
-      document.getElementById('itemName').value = '';
-      document.getElementById('itemPrice').value = '';
-  
-      document.getElementById('createForm').style.display = 'none';
-    } else {
-      alert('Please enter both name and price.');
-    }
-  }
-  
-  function sortItems(parameter) {
-    const items = getItems();
-  
-    items.sort((a, b) => {
-      if (a[parameter] < b[parameter]) return -1;
-      if (a[parameter] > b[parameter]) return 1;
-      return 0;
-    });
-  
-    setItems(items);
-    renderItems();
-  }
-  
-  
-  function searchItemsByName(name) {
-    const items = getItems();
-  
-    const filteredItems = items.filter(item => item.name.toLowerCase().includes(name.toLowerCase()));
-  
-    
-    renderItems(filteredItems);
-  }
-  
+    itemNameInput.value = '';
+    itemPriceInput.value = '';
+    itemCategoryInput.value = '';
 
-  function calculateTotalSum(parameter) {
-    const items = getItems();
-  
-    const totalSum = items.reduce((sum, item) => sum + item[parameter], 0);
-  
-    alert(`Total ${parameter}: $${totalSum}`);
+    document.getElementById('createForm').style.display = 'none';
+  } else {
+    alert('Please enter name, price, and category.');
   }
-  
-  const newItem = { name: itemName, price: parseFloat(itemPrice), imgSrc: 'path/to/image.jpg' };
-  li.innerHTML = `
-  <img src="${item.imgSrc}" alt="${item.name}" style="max-width: 100%;">
-  <p>${item.name} - $${item.price}</p>
-  <button class="edit" onclick="editItem(${item.id})">Edit</button>
-  <button class="delete" onclick="deleteItem(${item.id})">Delete</button>
-`;
+}
+
+function sortItems(parameter) {
+  const items = getItems();
+
+  items.sort((a, b) => {
+    const valueA = typeof a[parameter] === 'number' ? a[parameter] : a[parameter].toLowerCase();
+    const valueB = typeof b[parameter] === 'number' ? b[parameter] : b[parameter].toLowerCase();
+
+    if (valueA < valueB) return -1;
+    if (valueA > valueB) return 1;
+    return 0;
+  });
+
+  setItems(items);
+  renderItems();
+}
+
+
+function searchItemsByName(name) {
+  const items = getItems();
+
+  const filteredItems = items.filter(item => item.name.toLowerCase().includes(name.toLowerCase()));
+
+  renderItems(filteredItems);
+}
+
+function calculateTotalSum(parameter) {
+  const items = getItems();
+
+  const totalSum = items.reduce((sum, item) => sum + item[parameter], 0);
+
+  alert(`Total ${parameter}: $${totalSum}`);
+}
